3449a0e4a85acfa219546f872ce51c432cdd72d7
NicholasQuirky <nicholaskerky.victoriano.cics@ust.edu.ph>
2024-02-04 23:55:46 +0800

    Added pages Navbar & Admin Dashboard

---
+++ Admin/admindash.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
+  <title>Mosa Tire Supply</title>
+</head>
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Mosa Tire Supply</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+
+          <li class="nav-item">
+            <a class="nav-link" href="#">Content Page</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Inventory Page</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">View Orders</a>
+          </li>
+        </ul>
+        <span class="navbar-text">
+          Customer name
+        </span>
+
+        <ul class="navbar-nav">
+          <li class="nav-item logout">
+            <a class="nav-link" href="#">Logout</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container mt-5">
+
+      <button class="btn btn-primary btn-sm mx-2">Add Product</button>
+      <button class="btn btn-primary btn-sm mx-2">Update Product</button>
+      <button class="btn btn-primary btn-sm mx-2">Remove Product</button>
+      <button class="btn btn-primary btn-sm mx-2">View Product</button>
+
+
+  </div>
+
+  <div class="container mt-2">
+    <div class="row justify-content-center">
+      <button class="btn btn-primary btn-sm mx-1">View Inventory</button>
+    </div>
+
+  </div>
+
+
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
